953ac2310fea27ac5967878def0688986c7cba3b
HaiNhatTran <138964614+HaiNhatTran@users.noreply.github.com>
2024-06-06 01:21:12 +0700

    Update alu32bit.v

--- alu32bit.v
+++ alu32bit.v
@@ -31,21 +31,21 @@ output reg flagZ = 1'b0;
 parameter [3:0] ADD  = 4'b0000,   //CONG
 					 SUB  = 4'b0001,	//TRU
 					 MUL  = 4'b0010,	//MUL
-					 DIV  = 4'b0011,  //DIV
+					 DIV  = 4'b0011,        //DIV
 					 AND  = 4'b0100,	//AND
 					 OR   = 4'b0101,	//OR
 					 NAND = 4'b0110,	//NAND
 					 NOR  = 4'b0111,	//NOR
 					 XOR  = 4'b1000,	//XOR
 					 IN   = 4'b1001,	//Tang A len 1
-				    RE   = 4'b1010,	//Giam A di 1
+				         RE   = 4'b1010,	//Giam A di 1
 					 NOT  = 4'b1011,	//NOT				 
 					 //Phep toan logic
-					 SL   = 4'b1100, // Dich trai
-					 SR   = 4'b1101, //Dich phai
+					 SL   = 4'b1100, 	// Dich trai
+					 SR   = 4'b1101, 	//Dich phai
 					 //Phep toan so sanh
-					 CW   = 4'b1110, // So sanh bang
-					 CD   = 4'b1111; //So sanh HON
+					 CW   = 4'b1110, 	// So sanh bang
+					 CD   = 4'b1111; 	//So sanh HON
 																				
 always @(opcode or a or b)
 begin
@@ -53,102 +53,102 @@ begin
 		//adder
 		ADD: begin
 			result = a+b;
-			flagC  = result[31];
-			flagZ  = (result == 33'b0);
+			flagC  = result[32];
+			flagZ  = (result == 32'b0);
 		end
 		
 		//subtractor
 		SUB: begin
 			result = a-b;
-			flagC  = result[31];
-			flagZ  = (result == 33'b0);
+			flagC  = result[32];
+			flagZ  = (result == 32'b0);
 		end
 		
 		//Mach da hop
 		MUL: begin
 			result = a*b;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		//division
 		DIV: begin
 			result = a/b;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		//AND
 		AND: begin
 			result = a&b;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//OR
 		OR: begin
 			result = a|b;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//NAND
 		NAND: begin
 			result = ~(a&b);
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//NOR
 		NOR: begin
 			result = ~(a|b);
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//XOR
 		XOR: begin
 			result = a^b;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//Tang A len 1
 		IN: begin
 			result = a+1;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//Giam A xuong 1
 		RE: begin
 			result = a-1;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		// NOT
 		NOT: begin
 			result = ~a;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//Dich trai
 		SL: begin
 			result = a<<1;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//Dich phai
 		SR: begin
 			result = a>>1;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//So sang bang
 		CW: begin
 			result = (a == b) ? 1 : 0;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		
 		//So sanh khac
 		CD: begin
 			result = (a > b) ? 1 : 0;
-			flagZ  = (result == 33'b0);
+			flagZ  = (result == 32'b0);
 		end
 		default : begin
-			result = 33'b0;
-			flagC  = 33'b0;
-			flagZ  = 33'b0;
+			result = 32'b0;
+			flagC  = 1'b0;
+			flagZ  = 1'b0;
 		end
 	endcase
 end
